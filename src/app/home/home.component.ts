ded267713a2bf740296256d4ef54c89df5685b24
Amol S <amolsuryawanshiit@gmail.com>
2021-01-26 18:36:27 +0530

    did the changes in dynamic component loading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vcRef:ViewContainerRef,private resolver:ComponentFactoryResolver) { }
 
   ngOnInit(): void {
+    
+  }
+  async loadOrders(){
+    this.vcRef.clear();
+    const {OrdersComponent} = await import('../orders/orders.component');
+    this.vcRef.createComponent(this.resolver.resolveComponentFactory(OrdersComponent));
+    
   }
 
 }
